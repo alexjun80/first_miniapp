48c9d8e644239b6394eb08fefaccce6d47d45940
Alexey Sankin <alexjun@mail.ru>
2025-02-06 22:11:37 +0300

    Подключили скрипт из всяких инструкций, для Api

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  app: {
+    head: {
+      script: [
+        {
+          src: 'https://telegram.org/js/telegram-web-app.js',
+          type: 'text/javascript',
+          async: true,
+          defer: true
+        }
+      ]
+    }
+  }
+});
